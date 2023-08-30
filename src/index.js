172578936ab9a40f9079985e840ee0f35197c623
Vadim-27 <vadim.bulanyi@gmail.com>
2023-08-30 10:02:08 +0200

    fix2

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AuthLayout>
-          <BrowserRouter basename="goit-react-hw-08-phonebook">
+          <BrowserRouter basename="Phonebook">
             <App />
           </BrowserRouter>
         </AuthLayout>
